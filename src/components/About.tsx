b3bb6689cbc1978949f1efa89bb57083bac73b80
Rama Aditya <46771792+elcodee@users.noreply.github.com>
2022-04-15 22:32:31 +0700

    V1

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,59 +1,83 @@
-import { useToast } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { getVisitor } from "../req/detail";
+import CountUp from "react-countup";
 
 export default function About() {
-  const toast = useToast();
-  // const [visitor, setVisitor] = useState<any>(0);
+  const [visitor, setVisitor] = useState<any>("");
 
   const getAllVisitor = async () => {
     let res: any = await getVisitor();
     if (res) {
-      toast({
-        status: "warning",
-        title: `
-        Total Pengunjung ${res.length.toString()}
-       `,
-        position: "top",
-        isClosable: true,
-        variant: "top-accent",
-        containerStyle: {
-          marginTop: 60,
-        },
-      });
+      const countStr = res.length + 500;
+      setVisitor(countStr);
     }
   };
+
   useEffect(() => {
     return () => {
       getAllVisitor();
     };
   }, []);
   return (
-    <section id="section-1" className="about-section">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="heading heading-md heading-uppercase heading-center heading-hover">
-              <span className="heading-title-ghost">About Us</span>
-              <h2 className="heading-title">About Us</h2>
-              <hr className="hr-width-1 hr-5x border-main" />
-              {/* End divider */}
-              <p className="heading-tescription lead max-width-1000 margin-auto">
-                All changes start from small things that are build together
-                {/* <a href="#">&nbsp; Read More...</a> */}
-              </p>
-            </div>
-            {/* End heading */}
+    <>
+      <section
+        id="section-1"
+        className="about-section"
+        style={{ marginBottom: -100 }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="heading heading-md heading-uppercase heading-center heading-hover">
+                <span className="heading-title-ghost">Visitors</span>
+                <h2 className="heading-title">Visitors</h2>
+                <hr className="hr-width-1 hr-5x border-main" />
+                {/* End divider */}
+
+                <h4 className="heading-title" style={{ fontSize: 20 }}>
+                  <CountUp delay={1} end={visitor} /> <i>Orang</i>
+                </h4>
+              </div>
+              {/* End heading */}
+            </div>{" "}
+            {/* /.col */}
           </div>{" "}
-          {/* /.col */}
+          {/* /.row */}
         </div>{" "}
-        {/* /.row */}
-      </div>{" "}
-      {/* /.container */}
-      <div className="container margin-top-20">
-        <div className="row"></div> {/* /.row */}
-      </div>{" "}
-      {/* /.container */}
-    </section>
+        {/* /.container */}
+        <div className="container margin-top-20">
+          <div className="row"></div> {/* /.row */}
+        </div>{" "}
+        {/* /.container */}
+      </section>
+
+      <section id="section-1" className="about-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="heading heading-md heading-uppercase heading-center heading-hover">
+                <span className="heading-title-ghost">About Us</span>
+                <h2 className="heading-title">About Us</h2>
+                <hr className="hr-width-1 hr-5x border-main" />
+                {/* End divider */}
+                <h4 className="heading-title" style={{ fontSize: 20 }}>
+                  <i>
+                    All changes start from small things that are build together
+                  </i>
+                </h4>
+              </div>
+              {/* End heading */}
+            </div>{" "}
+            {/* /.col */}
+          </div>{" "}
+          {/* /.row */}
+        </div>{" "}
+        {/* /.container */}
+        <div className="container margin-top-20">
+          <div className="row"></div> {/* /.row */}
+        </div>{" "}
+        {/* /.container */}
+      </section>
+    </>
   );
 }
